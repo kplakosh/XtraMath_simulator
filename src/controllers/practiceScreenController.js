d75a5d6d2098cdd2a438013b6d5ee81b348f29ba
kplakosh <kplakosh@gmail.com>
2019-02-12 20:57:30 -0800

    set delay to keep answer printed on screen

--- src/controllers/practiceScreenController.js
+++ src/controllers/practiceScreenController.js
@@ -3,13 +3,12 @@ import PracticeModel from '../models/PracticeModel';
 
 export const startPractice = arr => {
     // Print new equation passing equations info
-    var practiceModel = new PracticeModel(0, arr[Math.floor(Math.random() * 100)], '', 0);
+    var practiceModel = new PracticeModel(0, chooseEquation(arr), '', 0);
 
     // Handel keypress 
-    window.addEventListener('keypress', event => {
+    window.addEventListener('keypress', async event => {
         practiceView.clearMiddle();
     
-        // Add key press to an array
         if (practiceModel.equation.value.length > practiceModel.input.length) {
             practiceModel.input += event.key;
         };
@@ -19,36 +18,24 @@ export const startPractice = arr => {
             if (practiceModel.input === practiceModel.equation.value) {
                 // If an input is correct update the equation status with success
                 practiceModel.equation.status = 'success';
-
-                // Update score
                 practiceModel.score += 1;
-
-                // Render equation including input
                 practiceView.renderEquation(practiceModel);
 
-                // Reset input
-                practiceModel.input = '';
+                await delay(1000);
 
-                // Reset timer
+                practiceModel.input = '';
                 practiceModel.seconds = 0;
 
-                // Go to next equation
-                practiceModel.equation = arr[Math.floor(Math.random() * 100)];
-
-                // Print equation with input
+                practiceModel.equation = chooseEquation(arr);
                 practiceView.renderEquation(practiceModel);
                 
             } else if (practiceModel.input != practiceModel.equation.value) {
-                // Update equation status with fail
                 practiceModel.equation.status = 'fail';
-
-                // If an input is not correct go to the main page
                 practiceView.clearMiddle();
             };
         } else if (practiceModel.input.length < practiceModel.equation.value.length) {
             // If an input is not full 
             if (practiceModel.input != practiceModel.equation.value) {
-                // If an input is not correct go to the main page
                 practiceView.clearMiddle();
                 practiceView.renderEquation(practiceModel);
             } else if (practiceModel.input === practiceModel.equation.value) {
@@ -57,6 +44,12 @@ export const startPractice = arr => {
             };
         };
     });
-    // Print equation
     practiceView.renderEquation(practiceModel);
+};
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+// Choose random equation
+const chooseEquation = arr => {
+    return arr[Math.floor(Math.random() * 100)];
 };
